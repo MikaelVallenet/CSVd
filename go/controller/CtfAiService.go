5a2841d2b941f49ca59a380326997a632c0f82b5
Mikatech <mikael.vallenet@epitech.eu>
2022-11-03 18:30:26 +0100

    feat(go): main launch API

--- go/controller/CtfAiService.go
+++ go/controller/CtfAiService.go
@@ -1,1 +1,30 @@
 package controller
+
+import (
+	"github.com/Mikatech/CTF-AI/database"
+	"github.com/Mikatech/CTF-AI/model"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+type NewUser struct {
+	Name     string `json:"name" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func getUsers(c *gin.Context) {
+
+	var users []model.User
+
+	db, err := database.Database()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if err := db.Find(&users).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, users)
+}
--- go/main.go
+++ go/main.go
@@ -1,1 +1,23 @@
-package _go
+package main
+
+import (
+	"github.com/Mikatech/CTF-AI/controller"
+	"github.com/Mikatech/CTF-AI/database"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+
+	db, err := database.Database()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	db.DB()
+
+	router := gin.Default()
+	
+	router.GET("/users", controller.getUsers)
+	log.Fatal(router.Run(":10000"))
+}
