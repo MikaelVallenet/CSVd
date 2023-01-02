0ad518421325d78b9365193162579d3d4b9d0e7e
Mikatech <mikael.vallenet@epitech.eu>
2023-01-02 18:04:41 +0100

    feat(web): add a login page

---
+++ web/src/pages/Login.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+const Login = () => {
+
+    return (
+        <div className="flex flex-col justify-center min-h-screen overflow-hidden">
+            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+                <h1 className="text-3xl font-semibold text-center text-blue-900 underline">
+                    Sign in
+                </h1>
+                <form className="mt-6">
+                    <div className="mb-2">
+                        <label
+                            htmlFor="email"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            className="block w-full px-4 py-2 mt-2 text-blue-900 bg-white border rounded-md focus:border-blue-900 focus:ring-blue-900 focus:outline-none focus:ring focus:ring-opacity-40"
+                            required
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            htmlFor="password"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            className="block w-full px-4 py-2 mt-2 text-blue-900 bg-white border rounded-md focus:border-blue-900 focus:ring-blue-900 focus:outline-none focus:ring focus:ring-opacity-40"
+                            required
+                        />
+                    </div>
+                    <div className="mt-6">
+                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-900 rounded-md hover:bg-blue-900 focus:outline-none focus:bg-blue-900">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login
