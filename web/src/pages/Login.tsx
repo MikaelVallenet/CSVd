5eb2f0335677f59c3e8335cc9cabea0f27210b3f
Mikatech <mikael.vallenet@epitech.eu>
2023-01-03 13:36:58 +0100

    feat(web): delete useless css file & switch color to cyan & adapt manifest.json

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -4,8 +4,8 @@ const Login = () => {
     return (
         <div className="flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-blue-900 underline">
-                    Sign in
+                <h1 className="text-3xl font-semibold text-center text-cyan-500 underline">
+                    CSVd
                 </h1>
                 <form className="mt-6">
                     <div className="mb-2">
@@ -17,7 +17,7 @@ const Login = () => {
                         </label>
                         <input
                             type="email"
-                            className="block w-full px-4 py-2 mt-2 text-blue-900 bg-white border rounded-md focus:border-blue-900 focus:ring-blue-900 focus:outline-none focus:ring focus:ring-opacity-40"
+                            className="block w-full px-4 py-2 mt-2 text-cyan-500 bg-white border rounded-md focus:border-cyan-500 focus:ring-cyan-500 focus:outline-none focus:ring focus:ring-opacity-40"
                             required
                         />
                     </div>
@@ -30,12 +30,12 @@ const Login = () => {
                         </label>
                         <input
                             type="password"
-                            className="block w-full px-4 py-2 mt-2 text-blue-900 bg-white border rounded-md focus:border-blue-900 focus:ring-blue-900 focus:outline-none focus:ring focus:ring-opacity-40"
+                            className="block w-full px-4 py-2 mt-2 text-cyan-500 bg-white border rounded-md focus:border-cyan-500 focus:ring-cyan-500 focus:outline-none focus:ring focus:ring-opacity-40"
                             required
                         />
                     </div>
                     <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-900 rounded-md hover:bg-blue-900 focus:outline-none focus:bg-blue-900">
+                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-cyan-500 rounded-md hover:bg-cyan-500 focus:outline-none focus:bg-cyan-500">
                             Login
                         </button>
                     </div>
