58f67fabff449b88e474124aee6caf429be09bad
Mikatech <mikael.vallenet@epitech.eu>
2023-01-03 13:54:14 +0100

    feat(web): add gradient background for login page

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -2,44 +2,46 @@ import React from 'react'
 const Login = () => {
 
     return (
-        <div className="flex flex-col justify-center min-h-screen overflow-hidden">
-            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-cyan-500">
-                    CSVd
-                </h1>
-                <form className="mt-6">
-                    <div className="mb-2">
-                        <label
-                            htmlFor="email"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            className="block w-full px-4 py-2 mt-2 text-cyan-500 bg-white border rounded-md focus:border-cyan-500 focus:ring-cyan-500 focus:outline-none focus:ring focus:ring-opacity-40"
-                            required
-                        />
-                    </div>
-                    <div className="mb-2">
-                        <label
-                            htmlFor="password"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            className="block w-full px-4 py-2 mt-2 text-cyan-500 bg-white border rounded-md focus:border-cyan-500 focus:ring-cyan-500 focus:outline-none focus:ring focus:ring-opacity-40"
-                            required
-                        />
-                    </div>
-                    <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-cyan-500 rounded-md hover:bg-cyan-500 focus:outline-none focus:bg-cyan-500">
-                            Login
-                        </button>
-                    </div>
-                </form>
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500">
+            <div className="flex flex-col justify-center min-h-screen overflow-hidden">
+                <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+                    <h1 className="text-3xl font-semibold text-center text-cyan-500">
+                        CSVd
+                    </h1>
+                    <form className="mt-6">
+                        <div className="mb-2">
+                            <label
+                                htmlFor="email"
+                                className="block text-sm font-semibold text-gray-800"
+                            >
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                className="block w-full px-4 py-2 mt-2 text-cyan-500 bg-white border rounded-md focus:border-cyan-500 focus:ring-cyan-500 focus:outline-none focus:ring focus:ring-opacity-40"
+                                required
+                            />
+                        </div>
+                        <div className="mb-2">
+                            <label
+                                htmlFor="password"
+                                className="block text-sm font-semibold text-gray-800"
+                            >
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                className="block w-full px-4 py-2 mt-2 text-cyan-500 bg-white border rounded-md focus:border-cyan-500 focus:ring-cyan-500 focus:outline-none focus:ring focus:ring-opacity-40"
+                                required
+                            />
+                        </div>
+                        <div className="mt-6">
+                            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-cyan-500 rounded-md hover:bg-cyan-500 focus:outline-none focus:bg-cyan-500">
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
