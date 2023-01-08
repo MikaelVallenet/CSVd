6ebb1a2486da632c0e5c8bc40b2990167ba85c16
Mikatech <mikael.vallenet@epitech.eu>
2023-01-08 21:13:23 +0100

    feat(front): add home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<Login />}/>
+            <Route path="/" element={<Login />}/>
+            <Route path="/home" element={<Home />}/>
+            <Route path="*" element={<h1>404</h1>}/>
         </Routes>
     </Router>
   );
